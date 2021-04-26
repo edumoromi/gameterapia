c25e1e94d6e8ca908505f014d85bc38ca1c98309
victormdsp <victormdsp@hotmail.com>
2021-04-26 18:49:42 -0300

    Mudanças iniciais

--- ClsHand.py
+++ ClsHand.py
@@ -1,6 +1,7 @@
 import pygame
 from ClsImage import Image
-
+from ClsGame import Game
+from ClsPizza import Pizza
 
 class Hand(pygame.sprite.Sprite):
     def __init__(self, startpos):
@@ -9,19 +10,23 @@ class Hand(pygame.sprite.Sprite):
         #self.direction = 1
         # #carrega a imagem e a posiciona na tela
         self.image, self.rect = Image.load_image("Mao1.jpg")
-        self.rect.centerx = startpos[0]
-        self.rect.centery = startpos[1]
+        self.image = pygame.transform.scale(self.image,(100,100)) #Muda o tamanho da imagem
+        self.rect.centerx = Game.DISPLAY_W
+        self.rect.centery = Game.DISPLAY_H - 100
         self.movex = 0
         self.movey = 0
 
+    #Controla a movimentação da mão
     def control(self, x, y):
         self.movex += x
         self.movey += y
+
+    #Verifica colisão com a tela
     def update(self):
-        if (self.rect.x <= 670) & (self.rect.x > -2):
+        if (self.rect.x <= Game.DISPLAY_W) & (self.rect.x > -2):
             self.rect.x = self.rect.x + self.movex
             self.rect.y = self.rect.y + self.movey
-            print(self.rect.x,self.rect.y)
+            #print(self.rect.x,self.rect.y)
         else:
             self.movex =0
             self.movey =0
@@ -45,13 +50,13 @@ class Hand(pygame.sprite.Sprite):
             if dir == "RIGHT":
                 if self.rect.x <= -2:
                     self.rect.x = 2
-                self.control(10,0)
+                self.control(2,0)
             else:
-                if self.rect.x >= 670:
-                    self.rect.x = 669
-                self.control(-10,0)
+                if self.rect.x >= Game.DISPLAY_W:
+                    self.rect.x = Game.DISPLAY_W
+                self.control(-2,0)
         elif key == "UP":
             if dir == "RIGHT":
-               self.control(-10,0)
+               self.control(-2,0)
             else:
-               self.control(10,0)
+               self.control(2,0)
--- ClsPizza.py
+++ ClsPizza.py
@@ -1,23 +1,17 @@
 import pygame
 from ClsImage import Image
-size = width, height = 6000, 4000
+size = width, height = 6000, 4000 #Não está sendo usado para nada
+
+def get():
+    tamanhoX = Pizza.get_rectx()
+    return tamanhoX
+
 class Pizza(pygame.sprite.Sprite):
     """classe para a bola"""
 
     def __init__(self, startpos):
         pygame.sprite.Sprite.__init__(self)
-        self.speed = [2, 2]
         self.image, self.rect = Image.load_image('pizza.jpg')
         self.rect.centerx = startpos[0]
         self.rect.centery = startpos[1]
         self.init_pos = startpos
-
-    def update(self,size):
-        self.rect.move_ip(self.speed)
-        if self.rect.left < 0 or self.rect.right > width:
-            self.speed[0] = -self.speed[0]
-        if self.rect.top < 0:
-            self.speed[1] = -self.speed[1]
-        if self.rect.bottom > size[1]:
-            self.rect.centerx = self.init_pos[0]
-            self.rect.centery = self.init_pos[1]
